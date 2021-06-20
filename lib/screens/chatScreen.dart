d792d1be9aabefdf1628f8c7c1de22b047477d0b
MorDvash <mordvash1@gmail.com>
2021-06-20 17:33:32 +0300

    Connecting firebase with flutter

--- lib/screens/chatScreen.dart
+++ lib/screens/chatScreen.dart
@@ -1,18 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-        navigationBar: const CupertinoNavigationBar(
-          middle: Text('WhatsApp'),
+      navigationBar: CupertinoNavigationBar(
+        middle: const Text('WhatsApp'),
+        trailing: CupertinoButton(
+          padding: EdgeInsets.zero,
+          onPressed: () {
+            FirebaseFirestore.instance
+                .collection('chats/YVPwJkQvvmcGl2mgw3OI/messages')
+                .snapshots()
+                .listen((data) {
+              data.docs.forEach((doc) {
+                print(doc['text']);
+              });
+            });
+          },
+          child: Icon(
+            CupertinoIcons.add,
+          ),
         ),
-        child: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, index) => Container(
-                  padding: EdgeInsets.all(8),
-                  child: Text('its work!!'),
-                )));
+      ),
+      child: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) => Container(
+          padding: EdgeInsets.all(8),
+          child: Text('its work!!'),
+        ),
+      ),
+    );
   }
 }
