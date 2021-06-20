3e3d8b76bb37c2d931e3ef1898a90f38cad6aabe
MorDvash <mordvash1@gmail.com>
2021-06-20 17:50:59 +0300

    Adding data

--- lib/screens/chatScreen.dart
+++ lib/screens/chatScreen.dart
@@ -1,36 +1,44 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: const Text('WhatsApp'),
-        trailing: CupertinoButton(
-          padding: EdgeInsets.zero,
-          onPressed: () {
-            FirebaseFirestore.instance
-                .collection('chats/YVPwJkQvvmcGl2mgw3OI/messages')
-                .snapshots()
-                .listen((data) {
-              data.docs.forEach((doc) {
-                print(doc['text']);
-              });
-            });
-          },
-          child: Icon(
-            CupertinoIcons.add,
+        navigationBar: CupertinoNavigationBar(
+          middle: const Text('WhatsApp'),
+          trailing: CupertinoButton(
+            padding: EdgeInsets.zero,
+            onPressed: () {
+              FirebaseFirestore.instance
+                  .collection('chats/YVPwJkQvvmcGl2mgw3OI/messages')
+                  .add({'text': 'some text'});
+            },
+            child: Icon(
+              CupertinoIcons.add,
+            ),
           ),
         ),
-      ),
-      child: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('its work!!'),
-        ),
-      ),
-    );
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('chats/YVPwJkQvvmcGl2mgw3OI/messages')
+              .snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) => Container(
+                padding: EdgeInsets.all(8),
+                child: Text(snapshot.data!.docs[index]['text']),
+              ),
+            );
+          },
+        ));
   }
 }
